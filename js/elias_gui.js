e0265f151b88b6be3575f3b8921eb3cf7ca044df
eliasvagan <elias.vagan@gmail.com>
2019-06-06 21:57:40 +0200

    Added camera shit

--- js/elias_gui.js
+++ js/elias_gui.js
@@ -6,12 +6,17 @@ class GUI {
         this.context = canvas.getContext("2d");
         this.width = canvas.width;
         this.height = canvas.height;
+        this.crosshair = {
+            thickness: 2,
+            size: 15,
+        };
         this.fps = {
             value: 0.0,
             posX: 5,
             posY: 15,
-            font: "monospace 10px",
-            color: "blue",
+            fontFace: "monospace",
+            fontSize: 40,
+            color: "yellow",
             rendering: true,
         };
     }
@@ -19,9 +24,23 @@ class GUI {
     render() {
         this.context.clearRect(0,0,this.width, this.height);
 
+        { // Crosshair
+            this.context.strokeStyle = "white";
+            this.context.lineWidth = this.crosshair.thickness;
+            this.context.moveTo(this.width / 2, this.height / 2 - this.crosshair.size / 2);
+            this.context.lineTo(this.width / 2, this.height / 2 + this.crosshair.size / 2);
+            this.context.moveTo(this.width / 2 - this.crosshair.size / 2, this.height / 2);
+            this.context.lineTo(this.width / 2 + this.crosshair.size / 2, this.height / 2);
+            this.context.stroke();
+        }
+
         if (this.fps.rendering) {
+            this.context.font = `${this.fps.fontFace} ${this.fps.fontSize}px`;
+            //FPS shadow
+            this.context.fillStyle = "black";
+            this.context.fillText(this.fps.value, this.fps.posX + this.fps.fontSize / 30, this.fps.posY + this.fps.fontSize / 30);
+            //FPS front
             this.context.fillStyle = this.fps.color;
-            this.context.font = this.fps.font;
             this.context.fillText(this.fps.value, this.fps.posX, this.fps.posY);
         }
     }
